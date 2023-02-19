f8e8ed27f516c87b224bf691f1b1081c52cbb40c
alexandria <alexandria.ame@gmail.com>
2023-02-19 03:28:36 -0800

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that prints s line with puts function
- * Return: 0(succses)
+ * main - Entry
+ *
+ * Return: Always 0 (success)
  */
-int main(void)
+int main (void) 
 {
-puts("\"Programming is like building a multilingual puzzle
-");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
