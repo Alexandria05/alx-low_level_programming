970647df3d92693e80115852a6562bcb8e8d85fd
alexandria <alexandria.ame@gmail.com>
2023-02-19 06:01:42 -0800

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 /*
- * File : 101-qoute.c 
+ * File: 101-quote.c
  * Auth: Brennan D Baraban
  */
 
 #include <unistd.h>
 
 /**
- *
- * main - Prints "and the piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
  *
  * Return: Always 1.
@@ -17,7 +16,6 @@ int main(void)
 	write(2,
 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 	      59);
-
 
 	return (1);
 }
