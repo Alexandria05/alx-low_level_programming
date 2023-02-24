21f0f8bfbe0ab2b4aab841633a01aa9869746911
alexandria <alexandria.ame@gmail.com>
2023-02-24 10:26:36 -0800

    hello

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
+
+
 /**
- * _isupper - A function that checks for uppercase character.
- * @c: An input character
+* @c: An input character
  * Return: 1 if c is uppercase or 0 otherwise
  */
 
